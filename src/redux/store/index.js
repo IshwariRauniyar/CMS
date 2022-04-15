175c34d4b21cd000b9f580979cd17f5664e9e517
Ishwari Rauniyar <ishirauniyar@gmail.com>
2022-04-15 16:09:54 +0545

    redux store

---
+++ src/redux/store/index.js
@@ -0,0 +1,7 @@
+import { applyMiddleware, createStore } from "redux";
+import rootReducers from "../reducers";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducers, applyMiddleware(thunk));
+
+export default store;
